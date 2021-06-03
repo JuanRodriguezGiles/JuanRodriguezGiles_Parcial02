83b3332d4a3ded5f050a66f7c5cbc79772dd8454
Juan <juanrodriguezgiles@gmail.com>
2021-06-03 12:51:25 -0300

    Implemented level creation

---
+++ Platformer/Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+public class LevelCreator : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject platform;
+
+    [SerializeField] int _rows;
+    [SerializeField] int _columns;
+    void Start()
+    {
+        BuildLevel();
+    }
+    void BuildLevel()
+    {
+        GameObject platformParent = new GameObject("Platforms");
+        GameObject go;
+        Vector3 position;
+
+        for (int rowIndex = 0; rowIndex < _rows; rowIndex++)
+        {
+            for (int columnIndex = 0; columnIndex < _columns; columnIndex++)
+            {
+                position = new Vector3(columnIndex, 0, rowIndex);
+                go = Instantiate(platform, position, Quaternion.identity, platformParent.transform);
+                go.name = "Platform";
+            }
+        }
+
+        int playerX = Mathf.RoundToInt(_columns / 2);
+        int playerZ = Mathf.RoundToInt(_rows / 2);
+        position = new Vector3(playerX, 0, playerZ);
+        go = Instantiate(player, position, Quaternion.identity);
+        go.name = "Player";
+    }
+}
